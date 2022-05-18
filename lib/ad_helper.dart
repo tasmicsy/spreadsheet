db2b7e8cf13b4f3adfb543464711b861c9aa93ff
Tasmicsy <obata.s.ac@gmail.com>
2022-05-18 19:00:56 +0900

    バナー広告本物に変更

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdHelper{
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-5943388097691889/2903327871';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-5943388097691889/6303630807';
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
